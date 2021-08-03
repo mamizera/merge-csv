98d0bc975287dcaede2ab76070283f8f77589076
mmizera <magdalena.mizera@wiosna.org.pl>
2021-08-03 10:03:48 +0200

    update script

--- merge-csv.py
+++ merge-csv.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pathlib import Path
 
 #-----ścieżka do plików-----
-path = str(Path(__file__).parent.absolute()) #jeśli pliki w tym samym folderze co skrypt
-#path = ""  #jeśli nie,, podajemy ścieżke do plików
+path = str(Path(__file__).parent.absolute())    #jeśli pliki w tym samym folderze co skrypt
+#path = ""   #jeśli nie,, podajemy ścieżke do plików
 
 filename1 = path + "\\file1.csv"
 filename2 = path + "\\file2.csv"
@@ -12,14 +12,12 @@ filename_res = "result.csv"
 
 #-----wczytaj dane--------
 print("reading data...")
-
 data1 = pd.read_csv(filename1)
 data2 = pd.read_csv(filename2)
 
 
 #-------mergowanie-------
 print("merging...")
-
 join_type = "inner"
 
 #jeśli klucz ta sama nazwa
@@ -34,7 +32,5 @@ out = pd.merge(data1, data2, on=key, how=join_type)
 
 #-------zapis do wynikowego csv-----------
 print("writing to file...")
-
 out.to_csv(filename_res, index=False)
-
 print("file saved")
